d3b132e1af255fe41723a6c4d1b7bd98dc335344
DaniGilMar <90446291+DaniGilMar@users.noreply.github.com>
2021-12-30 12:00:38 +0100

    Explicación_1ª_Lineas_CAmbio_Servidor_Desarrollo

--- Flask/app.py
+++ Flask/app.py
@@ -1,8 +1,19 @@
 from flask import Flask,render_template,url_for
-
+import os
 app = Flask(__name__)
-
+# Vamos a crear una lista para figurar una BBDD, y como pasar esta lista
+# rendereizado
+#1º Crear lista 
+empleados=["Ana","Maria","Sandra","Juan"]
 @app.route('/')
 
 def hola_mundo():
-    return render_template('index.html')
+    # 2º Para poder psar la info de la lista debemos
+    # hacerlo en forma de clave valor en el parámetro del render_template
+
+    return render_template('index.html', numeros_empleados = len(empleados))
+
+
+if __name__ == '__main__':
+    os.environ['FLASK_ENV'] = "development"
+    app.run(debug=True)
