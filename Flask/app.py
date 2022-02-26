595a9ac4d5116e30d9a4e829c6a335b9e47e86d7
DaniGilMar <90446291+DaniGilMar@users.noreply.github.com>
2022-02-26 12:57:46 +0100

    Nueva URL(Decorador-función)

--- Flask/app.py
+++ Flask/app.py
@@ -5,14 +5,17 @@ app = Flask(__name__)
 # rendereizado
 #1º Crear lista 
 empleados=["Ana","Maria","Sandra","Juan"]
-@app.route('/')
 
+
+@app.route('/')
 def hola_mundo():
     # 2º Para poder psar la info de la lista debemos
     # hacerlo en forma de clave valor en el parámetro del render_template
 
     return render_template('index.html', numeros_empleados = len(empleados))
-
+@app.route('/quienes')
+def quienes():
+    return "Esta es la página de quienes somos"
 
 if __name__ == '__main__':
     os.environ['FLASK_ENV'] = "development"
